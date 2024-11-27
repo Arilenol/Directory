f943ca88dbec76a90488cb875fe3b335de833dc7
Leasurew <aymeric.onfray@gmail.com>
2024-11-27 18:56:39 +0100

    correction de la boucle de saisie du menu  la fonction assert bloque le programme quand elle est fausse elle ne peut pas etre utiliser comme un condition de la boucle  while et la gestion des erreurs de type serra à traiter plus tard

--- source/main.c
+++ source/main.c
@@ -1,18 +1,20 @@
 #include "../head/head.h"
-
+#define NB_PROP 2
 int main(void){
     system("chcp 65001");
     system("cls");
-    char possible[2][10]={"Consulter","Ajouter"};
+    char possible[NB_PROP][10]={"Consulter","Ajouter"};
     int choix;
     printf("Que voulez-faire parmi les actions suivantes ? \n");
-    for (int i = 0; i<2;i++){
+    for (int i = 0; i<NB_PROP; i++){
         printf("%d) %s\n",i+1,possible[i]);
     }
     printf("\nEntrez le chiffre correspondant à l'action que vous souhaitez réaliser : ");
     scanf("%d",&choix);
-    while (assert(int choix==1) ){
-        printf("L'élément que vous avez rentré n'est pas un entier ! \n Réessayez : ");
+
+    while (choix<1 || choix >NB_PROP){
+        printf("Sasie hors plage\nRéessayez : ");
+        scanf("%d",&choix);
     }
     switch (choix)
     {
@@ -22,10 +24,6 @@ int main(void){
     
     case 2:
         //fonction Ajouter
-        break;
-    
-    default:
-        printf("\nVous avez rentré un nombre erroné");
         break;
     }
 
